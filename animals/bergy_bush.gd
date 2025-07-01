abaaf638193defa4b13116f6a8d81be9bf780c19
Jasper Raynolds <jaspirian@gmail.com>
2025-06-30 21:56:27 -0700

    Bergybush

---
+++ animals/bergy_bush.gd
@@ -0,0 +1,21 @@
+extends Animal
+class_name BergyBush
+
+var watered : bool = false
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+func _physics():
+	#for animal in Map.animal_container.get_children():
+		#if animal is Water:
+			#if  position.distance_to(animal.position)< 16:
+				#watered = true
+				#return
+	#watered = false
+	return
