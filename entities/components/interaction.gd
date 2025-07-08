61f430f8b2afe719bfc57e286502ef49fc5e28f7
scholei <fuso155@gmail.com>
2025-07-07 18:13:38 -0700

    Added Bergy Detection

--- entities/components/interaction.gd
+++ entities/components/interaction.gd
@@ -1,3 +1,12 @@
-extends Node2D
+extends Area2D
 class_name Interaction
 ## Class for an animal's interactions.
+
+
+func _on_body_entered(body):
+	print("Entered")
+	pass # Replace with function body.
+
+func _on_body_exited(body):
+	print("Exited")
+	pass # Replace with function body.
